403dfd4e33f0dccce53f0d0bc2cca01fafff8907
Christopher Mooney <chris@dod.net>
2020-03-21 15:14:41 -0400

    fix whitespace bug

--- flop/model-mkr.sh
+++ flop/model-mkr.sh
@@ -5,10 +5,12 @@ while true; do
   source env/environment.sh  # share ETH_URL, DISCOUNT, and GASPRICE
 
   # dynamic gas price
-  res=$(curl -s -X POST \                                                               -H "Content-Type: application/json" \
+  res=$(curl -s -X POST \
+    -H "Content-Type: application/json" \
     --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[],\"id\":${RANDOM}}" \
     http://${SERVER_ETH_RPC_HOST}:${SERVER_ETH_RPC_PORT} \
-    | jq -r '.result' \                                                                 | sed 's/0x//')
+    | jq -r '.result' \
+    | sed 's/0x//')
   gas=$(bc <<<"ibase=16; $(tr a-f A-F <<<"${res}")")
   gasPrice=$(bc -l <<< "scale=0; (${gas} * (1 + ${GAS_PERCENT}))/1")
 
