cb96cdecea2252d9592e5a2238e11e074e1b76e0
Christopher Mooney <chris@dod.net>
2020-03-21 15:06:42 -0400

    updated models to use dynamic gas

--- flip/eth-a/model-eth.sh
+++ flip/eth-a/model-eth.sh
@@ -2,15 +2,24 @@
 
 while true; do
 
-   source env/environment.sh  # share ETH_URL, DISCOUNT, and GASPRICE
+  source env/environment.sh  # share ETH_URL, DISCOUNT, and GASPRICE
 
-   body=$(curl -s -X GET "$FLIP_ETH_URL" -H "accept: application/json")
+  # dynamic gas price
+  res=$(curl -s -X POST \
+    -H "Content-Type: application/json" \
+    --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[],\"id\":${RANDOM}}" \
+    http://${SERVER_ETH_RPC_HOST}:${SERVER_ETH_RPC_PORT} \
+    | jq -r '.result' \
+    | sed 's/0x//')
+  gas=$(bc <<<"ibase=16; $(tr a-f A-F <<<"${res}")")
+  gasPrice=$(bc -l <<< "scale=0; (${gas} * (1 + ${GAS_PERCENT}))/1")
 
-   ethPrice=$(echo $body | jq '.ethereum.usd')
+  # dynamic bid price
+  body=$(curl -s -X GET "${FLIP_ETH_URL}" -H "accept: application/json")
+  ethPrice=$(echo ${body} | jq '.ethereum.usd')
+  bidPrice=$(bc -l <<< "${ethPrice} * (1-${FLIP_ETH_DISCOUNT})")
 
-   bidPrice=$(bc -l <<< "$ethPrice * (1-$FLIP_ETH_DISCOUNT)")
+  echo "{\"price\": \"${bidPrice}\", \"gasPrice\": \"${gasPrice}\"}"
 
-   echo "{\"price\": \"${bidPrice}\", \"gasPrice\": \"${FLIP_GASPRICE}\"}"
-
-   sleep 25
+  sleep 25
 done
--- flop/model-mkr.sh
+++ flop/model-mkr.sh
@@ -2,15 +2,22 @@
 
 while true; do
 
-   source env/environment.sh  # share ETH_URL, DISCOUNT, and GASPRICE
+  source env/environment.sh  # share ETH_URL, DISCOUNT, and GASPRICE
 
-   body=$(curl -s -X GET "$FLOP_MKR_URL" -H "accept: application/json")
+  # dynamic gas price
+  res=$(curl -s -X POST \                                                               -H "Content-Type: application/json" \
+    --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[],\"id\":${RANDOM}}" \
+    http://${SERVER_ETH_RPC_HOST}:${SERVER_ETH_RPC_PORT} \
+    | jq -r '.result' \                                                                 | sed 's/0x//')
+  gas=$(bc <<<"ibase=16; $(tr a-f A-F <<<"${res}")")
+  gasPrice=$(bc -l <<< "scale=0; (${gas} * (1 + ${GAS_PERCENT}))/1")
 
-   mkrPrice=$(echo $body | jq '.maker.usd')
+  # dynamic bid price
+  body=$(curl -s -X GET "${FLOP_MKR_URL}" -H "accept: application/json")
+  mkrPrice=$(echo $body | jq '.maker.usd')
+  bidPrice=$(bc -l <<< "${mkrPrice} * (1-${FLOP_MKR_DISCOUNT})")
 
-   bidPrice=$(bc -l <<< "$mkrPrice * (1-$FLOP_MKR_DISCOUNT)")
+  echo "{\"price\": \"${bidPrice}\", \"gasPrice\": \"${gasPrice}\"}"
 
-   echo "{\"price\": \"${bidPrice}\", \"gasPrice\": \"${FLOP_GASPRICE}\"}"
-
-   sleep 25
+  sleep 25
 done
