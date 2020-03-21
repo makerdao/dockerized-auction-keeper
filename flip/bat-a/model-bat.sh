b8d57d4af6aeb2d12842e39fb548c515cdb3dab2
Christopher Mooney <chris@dod.net>
2020-03-21 15:47:35 -0400

    dynamic gas model for BAT

--- flip/bat-a/model-bat.sh
+++ flip/bat-a/model-bat.sh
@@ -2,15 +2,24 @@
 
 while true; do
 
-   source ../env/environment.sh # share BAT_URL, DISCOUNT, and GASPRICE
+  source ../env/environment.sh # share BAT_URL, DISCOUNT, and GASPRICE
 
-   body=$(curl -s -X GET "$FLIP_BAT_URL" -H "accept: application/json")
+  # dynamic gas price
+  res=$(curl -s -X POST \
+    -H "Content-Type: application/json" \
+    --data "{\"jsonrpc\":\"2.0\",\"method\":\"eth_gasPrice\",\"params\":[],\"id\":${RANDOM}}" \
+    http://${SERVER_ETH_RPC_HOST}:${SERVER_ETH_RPC_PORT} \
+    | jq -r '.result' \
+    | sed 's/0x//')
+  gas=$(bc <<<"ibase=16; $(tr a-f A-F <<<"${res}")")
+  gasPrice=$(bc -l <<< "scale=0; (${gas} * (1 + ${GAS_PERCENT}))/1")
 
-   batPrice=$(echo $body | jq '."basic-attention-token".usd')
+  # dynamic bid price
+  body=$(curl -s -X GET "$FLIP_BAT_URL" -H "accept: application/json")
+  batPrice=$(echo $body | jq '."basic-attention-token".usd')
+  bidPrice=$(bc -l <<< "$batPrice * (1-$FLIP_BAT_DISCOUNT)")
 
-   bidPrice=$(bc -l <<< "$batPrice * (1-$FLIP_BAT_DISCOUNT)")
+  echo "{\"price\": \"${bidPrice}\", \"gasPrice\": \"${FLIP_GASPRICE}\"}"
 
-   echo "{\"price\": \"${bidPrice}\", \"gasPrice\": \"${FLIP_GASPRICE}\"}"
-
-   sleep 25
+  sleep 25
 done
