1eb37a6ce4b74e389949dadaada008c139bbf04b
grandizzy <grandizzy@localhost>
2020-04-07 15:30:47 +0300

    Use http://localhost:8545 in environment.sh

--- env/environment.sh
+++ env/environment.sh
@@ -1,5 +1,5 @@
 # host and port
-SERVER_ETH_RPC_HOST=https://paritynodehere:8545
+SERVER_ETH_RPC_HOST=https://localhost:8545
 
 # Dynamic Gas Price Model
 # 0 = get the gas price from the node (default)
